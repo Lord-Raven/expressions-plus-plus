9aadc0e42efea7c09e6c1faa213d77a44ddeff17
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-08-04 01:48:00 +0000

    Depth fiddling.

--- src/SpeakerImage.tsx
+++ src/SpeakerImage.tsx
@@ -49,7 +49,7 @@ const SpeakerImage: FC<SpeakerImageProps> = ({
     }, []);
 
     // Calculate final parallax position
-    const depth = yPosition * DEPTH;
+    const depth = (50 - yPosition) * DEPTH;
     const finalX = (isTalking ? 50 : xPosition) + (alphaMode ? (- mousePosition.x * depth * PARALLAX_STRENGTH) : 0);
     const finalY = (isTalking ? (2 + yPosition) : (4 + yPosition)) + (alphaMode ? (- mousePosition.y * depth * PARALLAX_STRENGTH) : 0);
     if (alphaMode) {
