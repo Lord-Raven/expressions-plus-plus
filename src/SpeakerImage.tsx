afa29b1b1d47d69296f0235a905f638d98081d71
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-08-09 01:17:29 +0000

    Messing with blend.

--- src/SpeakerImage.tsx
+++ src/SpeakerImage.tsx
@@ -108,17 +108,15 @@ const SpeakerImage: FC<SpeakerImageProps> = ({
                     exit='absent'
                     animate={currentState}
                     style={{position: 'absolute', width: 'auto', aspectRatio: '9 / 16', overflow: 'visible'}}>
-                <img src={imageUrl} style={{position: 'absolute', top: 0, width: '100%', height: '100%', filter: 'blur(2.5px)', transform: 'translate(-50%, 0)', zIndex: 4}} alt={`${speaker.name} (${emotion})`}/>
-                <img src={imageUrl} style={{position: 'absolute', top: 0, width: '100%', height: '100%', opacity: 0.75, transform: 'translate(-50%, 0)', zIndex: 5}} alt={`${speaker.name} (${emotion})`}/>
+                <div style={{backgroundImage: `url(${imageUrl})`, position: 'absolute', top: 0, width: '100%', height: '100%', filter: 'blur(2.5px)', transform: 'translate(-50%, 0)', zIndex: 4}}/>
+                <div style={{backgroundImage: `url(${imageUrl})`, position: 'absolute', top: 0, width: '100%', height: '100%', opacity: 0.75, transform: 'translate(-50%, 0)', zIndex: 5}}/>
                 <div style={{
                     position: 'absolute',
                     top: 0,
-                    left: 0,
                     width: '100%',
                     height: '100%',
                     backgroundColor: highlightColor,
                     mixBlendMode: 'multiply',
-                    filter: 'blur(2.5px)',
                     transform: 'translate(-50%, 0)',
                     zIndex: 6
                 }} />
